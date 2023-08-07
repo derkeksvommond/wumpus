7e11f23267bf57977a2b92786e7ca9ede9a98357
Mondkeks <81697210+derkeksvommond@users.noreply.github.com>
2023-08-07 17:02:30 +0200

    Update object.py

--- wumpus/object.py
+++ wumpus/object.py
@@ -1,12 +1,12 @@
 class Object:
+    
     attributes = list()
 
-    def __init__(self) -> None:
-        pass
+    def __init__(**kwargs) -> None:
+        for kwarg in kwargs:
+            self.
 
-    """
     def __iter__(self) -> dict:
         return iter(dict(
             ((key for key in attributes))
         ).items())
-    """
