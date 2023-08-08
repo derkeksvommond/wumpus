9bfbe77ea303f66399c19912585356f6c2c58f1e
Mondkeks <81697210+derkeksvommond@users.noreply.github.com>
2023-08-08 16:09:23 +0200

    Update object.py

--- wumpus/object.py
+++ wumpus/object.py
@@ -1,12 +1,20 @@
 class Object:
-    
-    attributes = list()
-
-    def __init__(**kwargs) -> None:
-        for kwarg in kwargs:
-            self.
 
     def __iter__(self) -> dict:
-        return iter(dict(
-            ((key for key in attributes))
-        ).items())
+
+        attributes = [attribut for attribut in dir(self) if not callable(getattr(self, attribut)) and not attribut.startswith("__")]
+        values = self.__dict__
+
+        payload = dict()
+
+        for key in attributes:
+
+            payload[key] = (
+                None if values[key] is None 
+                else dict(values[key]) if isinstance(values[key], Object)
+                else [ (dict(_) if isinstance(_, Object) else _) for _ in values[key] ] if isinstance(values[key], list)
+                else values[key]
+            )
+            
+
+        return iter(payload.items())
