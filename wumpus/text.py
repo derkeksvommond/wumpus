d63fc1232c18b94092d184fb8c074c7033098c45
Mondkeks <81697210+derkeksvommond@users.noreply.github.com>
2023-09-22 21:33:41 +0200

    Update text.py

--- wumpus/text.py
+++ wumpus/text.py
@@ -1,24 +1,12 @@
 from typing import Union
+
+from object import Object
+from components import *
+from message import Message
+
 import json
 
-# from .channel import Message
-
-class Message:
-    id = 123456789
-
-
-class Sticker:
-    pass
-
-
-class Component:
-    pass
-
-
-class Button(Component):
-    pass
-
-class Provider:
+class Provider(Object):
     def __init__(
         self,
         name: str = None,
@@ -26,14 +14,8 @@ class Provider:
     ) -> None:
         self.name = name
         self.url = url
-
-    def __iter__(self) -> dict:
-        return iter(dict(
-            name = self.name,
-            url = self.url
-        ).items())
         
-class Footer:
+class Footer(Object):
     def __init__(
             self,
             text: str,
@@ -45,14 +27,7 @@ class Footer:
         self.icon_url = icon_url
         self.proxy_icon_url = proxy_icon_url
 
-    def __iter__(self) -> dict:
-        return iter(dict(
-            text = self.text,
-            icon_url = self.icon_url,
-            proxy_icon_url = self.proxy_icon_url
-        ).items())
-
-class Author:
+class Author(Object):
     def __init__(
         self,
         name: str,
@@ -66,15 +41,7 @@ class Author:
         self.icon_url = icon_url
         self.proxy_icon_url = proxy_icon_url
 
-    def __iter__(self) -> dict:
-        return iter(dict(
-            name = self.name,
-            url = self.url,
-            icon_url = self.icon_url,
-            proxy_icon_url = self.proxy_icon_url
-        ).items())
-
-class Field:
+class Field(Object):
     def __init__(
         self,
         name: str = None,
@@ -86,14 +53,7 @@ class Field:
         self.value = value
         self.inline = inline
 
-    def __iter__(self) -> dict:
-        return iter(dict(
-            name = self.name,
-            value = self.value,
-            inline = self.inline
-        ).items())
-
-class Image:
+class Image(Object):
     def __init__(
             self,
             url: str,
@@ -107,17 +67,9 @@ class Image:
         self.height = height
         self.width = width
 
-    def __iter__(self) -> dict:
-        return iter(dict(
-            url = self.url,
-            proxy_url = self.proxy_url,
-            height = self.height,
-            width = self.width
-        ).items())
-    
 Video = Image
 
-class Embed:
+class Embed(Object):
 
     def __init__(
         self,
@@ -125,7 +77,7 @@ class Embed:
         description: str = None,
         type: str = "rich",
         url: str = None,
-        timestamp: str = "2069-04-20T00:00:00+0000",
+        timestamp: str = None,
         color: int = None,
         footer: Footer = None,
         image: Image = None,
@@ -148,38 +100,18 @@ class Embed:
         self.author = author
         self.fields = fields
 
-    def __iter__(self) -> dict:
-        return iter(dict(
-            title = self.title,
-            type = self.type,
-            description = self.description,
-            url = self.url,
-            timestamp = self.timestamp,
-            color = self.color,
-            
-            provider = None if self.provider is None else dict(self.provider),
-            image = None if self.image is None else dict(self.image),
-            video = None if self.video is None else dict(self.video),
-            footer = None if self.footer is None else dict(self.footer),
-            author = None if self.author is None else dict(self.author),
-            
-            fields = [] if not self.fields else [ dict(field) for field in self.fields ]
-
-        ).items())
-
-
-class Text:
+class Text(Object):
 
     def __init__(
         self,
         content: str,
         embeds: list[Embed] = [],
-        components: list[Component] = [],
-        nonce: str = "undefined",
+        components: list[Component] = None,
+        nonce: str = None,
         tts: bool = False,
         # allowed_mentions
         message_reference: Union[Message, int] = None,
-        sticker_ids: list[Union[Sticker, int]] = [],
+        sticker_ids: list[int] = [],
         # files
         # payload_json
         # attachments
@@ -190,45 +122,10 @@ class Text:
         self.tts = tts
         self.embeds = embeds
         self.message_reference = message_reference
-        self.components = components
         self.sticker_ids = sticker_ids
         self.flags = flags
 
-    def __iter__(self) -> dict:
-        
-        return iter(dict(
-            content = self.content,
-            nonce = self.nonce,
-            tts = self.tts,
-            flags = self.flags,
-            sticker_ids = self.sticker_ids,
-
-            embeds = ( [] if not self.embeds else [ dict(embed) for embed in self.embeds ] ),
-            components = ( [] if not self.components else [ dict(component) for component in self.components ] ),
-            
-            message_reference = ( 
-                None
-                    if self.message_reference is None 
-                    else ( self.message_reference
-                        if type(self.message_reference) is int 
-                        else self.message_reference.id
-                    )
-            )
-        ).items())
-
-
-
-
-print(json.dumps(dict(Text(
-    "Hallo Welt, das ist meine allererste Nachricht :)",
-    embeds=[
-        Embed(
-            "Titel",
-            "Wie es sich für eine erste Nachricht gehört, ist hier etwas Text.",
-            fields=[
-                Field("Fußballfeld","Was ist das überhaupt??")
-            ]
-        ),
-        Embed("Kein Titel?", "Doch!")
-    ]
-)), indent=4))
+        self.components = [dict(
+            type=1,
+            components=[component.json() for component in components]
+        )] if components is not None else None
