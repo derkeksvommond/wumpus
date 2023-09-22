57816ae175ed33929a52b2ceb47fc879a7bb692e
Mondkeks <81697210+derkeksvommond@users.noreply.github.com>
2023-09-22 21:33:15 +0200

    Update object.py

--- wumpus/object.py
+++ wumpus/object.py
@@ -1,20 +1,23 @@
 class Object:
 
-    def __iter__(self) -> dict:
+    def json(self) -> dict:
 
-        attributes = [attribut for attribut in dir(self) if not callable(getattr(self, attribut)) and not attribut.startswith("__")]
+        attributes = [
+            attribut for attribut in dir(self)
+            if not callable(getattr(self, attribut))
+            and not attribut.startswith("_")
+        ]
+
         values = self.__dict__
-
         payload = dict()
 
         for key in attributes:
 
             payload[key] = (
-                None if values[key] is None 
-                else dict(values[key]) if isinstance(values[key], Object)
-                else [ (dict(_) if isinstance(_, Object) else _) for _ in values[key] ] if isinstance(values[key], list)
+                None if values[key] is None
+                else values[key].json() if isinstance(values[key], Object)
+                else [ (_.json() if isinstance(_, Object) else _) for _ in values[key] ] if isinstance(values[key], list)
                 else values[key]
             )
-            
 
-        return iter(payload.items())
+        return payload
